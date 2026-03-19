728cbcc4b55111f1d7582e16a2bef35694015e77
Dev <9416650+aoinoikaz@users.noreply.github.com>
2026-03-19 12:02:52 -0400

    Fix default.nix: add nova-cache overlay from GitHub main

--- default.nix
+++ default.nix
@@ -1,3 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
-pkgs.haskellPackages.callCabal2nix "nova-nix" ./. {}
+let
+  nova-cache-src = builtins.fetchGit {
+    url = "https://github.com/Novavero-AI/nova-cache.git";
+    ref = "main";
+  };
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: {
+      nova-cache = self.callCabal2nix "nova-cache" nova-cache-src {};
+    };
+  };
+in
+  haskellPackages.callCabal2nix "nova-nix" ./. {}
